694f4542e8a934e1f2e71ac946b1b10a02f6a611
ColiJD <colindresj9@gmail.com>
2024-10-11 14:36:05 -0600

    Version0.1 de carwashtresvalles

---
+++ SistemaVenta.DAL/DBContext/DBVENTAContext.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SistemaVenta.Entity;
+
+namespace SistemaVenta.DAL.DBContext
+{
+    public partial class DBVENTAContext : DbContext
+    {
+        public DBVENTAContext()
+        {
+        }
+
+        public DBVENTAContext(DbContextOptions<DBVENTAContext> options)
+            : base(options)
+        {
+        }
+
+
+        public virtual DbSet<Menu> Menus { get; set; } = null!;
+        
+        public virtual DbSet<Rol> Rols { get; set; } = null!;
+        public virtual DbSet<RolMenu> RolMenus { get; set; } = null!;
+       
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Menu>(entity =>
+            {
+                entity.HasKey(e => e.IdMenu)
+                    .HasName("PK__Menu__C26AF483CA54C183");
+
+                entity.ToTable("Menu");
+
+                entity.Property(e => e.IdMenu).HasColumnName("idMenu");
+
+                entity.Property(e => e.Controlador)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("controlador");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("descripcion");
+
+                entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Icono)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("icono");
+
+                entity.Property(e => e.IdMenuPadre).HasColumnName("idMenuPadre");
+
+                entity.Property(e => e.PaginaAccion)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("paginaAccion");
+
+                entity.HasOne(d => d.IdMenuPadreNavigation)
+                    .WithMany(p => p.InverseIdMenuPadreNavigation)
+                    .HasForeignKey(d => d.IdMenuPadre)
+                    .HasConstraintName("FK__Menu__idMenuPadr__108B795B");
+            });
+
+            
+
+            modelBuilder.Entity<Rol>(entity =>
+            {
+                entity.HasKey(e => e.IdRol)
+                    .HasName("PK__Rol__3C872F763C1D1497");
+
+                entity.ToTable("Rol");
+
+                entity.Property(e => e.IdRol).HasColumnName("idRol");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("descripcion");
+
+                entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<RolMenu>(entity =>
+            {
+                entity.HasKey(e => e.IdRolMenu)
+                    .HasName("PK__RolMenu__CD2045D8F6A2531C");
+
+                entity.ToTable("RolMenu");
+
+                entity.Property(e => e.IdRolMenu).HasColumnName("idRolMenu");
+
+                entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.IdMenu).HasColumnName("idMenu");
+
+                entity.Property(e => e.IdRol).HasColumnName("idRol");
+
+                entity.HasOne(d => d.IdMenuNavigation)
+                    .WithMany(p => p.RolMenus)
+                    .HasForeignKey(d => d.IdMenu)
+                    .HasConstraintName("FK__RolMenu__idMenu__182C9B23");
+
+                entity.HasOne(d => d.IdRolNavigation)
+                    .WithMany(p => p.RolMenus)
+                    .HasForeignKey(d => d.IdRol)
+                    .HasConstraintName("FK__RolMenu__idRol__173876EA");
+            });
+
+           
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuario__645723A6E5D3BBB4");
+
+                entity.ToTable("Usuario");
+
+                entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+
+                entity.Property(e => e.Clave)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("clave");
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("correo");
+
+                entity.Property(e => e.EsActivo).HasColumnName("esActivo");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaRegistro")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.IdRol).HasColumnName("idRol");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+
+
+                entity.HasOne(d => d.IdRolNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdRol)
+                    .HasConstraintName("FK__Usuario__idRol__1BFD2C07");
+            });
+
+            
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ SistemaVenta.Entity/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaVenta.Entity
+{
+    public partial class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Usuario1 { get; set; } = null!;
+        public string Nombre { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+        public int IdRol { get; set; }
+        public string Clave { get; set; } = null!;
+        public bool EsActivo { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+
+        public virtual Rol IdRolNavigation { get; set; } = null!;
+    }
+}
